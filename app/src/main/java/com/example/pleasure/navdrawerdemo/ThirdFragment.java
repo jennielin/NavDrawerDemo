27d7c6a7de3dfa23f1e387a6a635f96de52f68aa
Jennie Lin <jennie88@gmail.com>
2015-03-11 20:48:38 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pleasure/navdrawerdemo/ThirdFragment.java
@@ -0,0 +1,9 @@
+package com.example.pleasure.navdrawerdemo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Pleasure on 3/11/2015.
+ */
+public class ThirdFragment extends Fragment {
+}
